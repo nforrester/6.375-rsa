489a02d30b48603a1c12de047b3a2bf6a18a338b
onasafari <anders.ariel@gmail.com>
2013-04-03 14:23:40 -0400

    added rsa files

---
+++ c_code/rsa.h
@@ -0,0 +1,10 @@
+/* rsa.h
+*
+* This headerfile will define the function interfaces...tbd
+*/
+
+#define SUCCESS 0
+#define FAIL 1
+
+// mult function returns SUCCESS or FAIL 
+int mult(int* a, int a_len, int* b, int b_len, int* res, int* res_len);
