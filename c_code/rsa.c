fe5cb9fb044e9c3dbacba4b7ede4a684cb187af0
Neil Forrester <nforrest@mit.edu>
2013-04-03 16:00:09 -0400

    modExpt implemented, plus functions for equality testing, and setting to 0

--- c_code/rsa.c
+++ c_code/rsa.c
@@ -2,8 +2,37 @@
 #include "util.h"
 #include "rsa.h"
 
+// result = b ^ e % m
+int modExpt(bigint b, bigint e, bigint m, bigint *result) {
+  bigint zero;
+  clearBigint(zero);
 
-// mult function returns SUCCESS or FAIL 
-int mult(int* a, int a_len, int* b, int b_len, int* res, int* res_len){
-  return SUCCESS;
+  clearBigint(result);
+  result->data[0] = 1;
+
+  while (!equal(zero, e)) {
+    if (e.data[0] % 2 == 1) {
+      modMult(result, b, m, &result);
+    }
+    modMult(b, b, m, &b);
+    shiftR(e, 2, &e);
   }
+  return SUCCESS;
+}
+
+// a = 0
+void clearBigint(bigint *a) {
+  for (int i = 0; i < NCHUNKS; i++) {
+    a->data[i] = 0;
+  }
+}
+
+// a == b
+int equal(bigint a, bigint b) {
+  for (int i = 0; i < NCHUNKS; i++) {
+    if (a.data[i] != b.data[i]) {
+      return FALSE;
+    }
+  }
+  return TRUE;
+}
--- c_code/rsa.h
+++ c_code/rsa.h
@@ -6,6 +6,9 @@
 #define SUCCESS 0
 #define FAIL 1
 
+#define TRUE 1
+#define FALSE 0
+
 #define BI_SIZE 1024
 #define CHUNK_T uint16_t
 #define NCHUNKS BI_SIZE / (sizeof(CHUNK_T))
@@ -19,6 +22,9 @@ int modMultNaive(bigint a, bigint b, bigint m, bigint *result);
 int modMultIlvd(bigint a, bigint b, bigint m, bigint *result);
 int modMultMagic(bigint a, bigint b, bigint m, bigint *result);
 
+// whichever implementation is best
+#define modMult modMultMagic
+
 // result = a << s
 int shiftL(bigint a, unsigned int s, bigint *result);
 
@@ -27,3 +33,9 @@ int shiftR(bigint a, unsigned int s, bigint *result);
 
 // result = b ^ e % m
 int modExpt(bigint b, bigint e, bigint m, bigint *result);
+
+// a == b
+int equal(bigint a, bigint b);
+
+// a = 0
+void clearBigint(bigint *a);
